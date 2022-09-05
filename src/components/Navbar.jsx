7114f36bbf0197b8009b17d159555b71960391c7
Paulina Cole <lina.cole@gmail.com>
2022-09-05 18:28:53 -0400

    created nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const style = {
+    nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+    heading: `text-white text-3xl`
+}
+
+export default function Navbar() {
+  return (
+    <div className={style.nav}>
+        <h1 className={style.heading}>Chat App</h1>
+
+    </div>
+  )
+}
