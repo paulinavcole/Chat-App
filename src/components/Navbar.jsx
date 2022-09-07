6ac2223b0bf3ff80ec3aca8f8f8613ffbc8ddb8d
Paulina Cole <lina.cole@gmail.com>
2022-09-07 11:17:50 -0400

    log out component created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import {auth} from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import SignIn from './SignIn'
+import LogOut from './LogOut'
 
 const style = {
     nav: `bg-blue-800 h-20 flex justify-between items-center p-4`,
     heading: `text-white text-3xl`
 }
 
-export default function Navbar() {
+const Navbar = () => {
 
   const [user] = useAuthState(auth)
   console.log(user)
   return (
     <div className={style.nav}>
         <h1 className={style.heading}>Chat App</h1>
-        <SignIn />
+        {user ? <LogOut /> : <SignIn />}
     </div>
   )
 }
+
+export default Navbar
