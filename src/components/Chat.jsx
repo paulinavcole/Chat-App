d9d5ef8bab2811cce3976fd3bc5a7a591df6332e
Paulina Cole <lina.cole@gmail.com>
2022-09-07 12:46:41 -0400

    message component complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react'
 import { Message } from './Message'
 import {query, collection, orderBy, onSnapshot} from 'firebase/firestore'
 import {db} from '../firebase'
+import { SendMessage } from './SendMessage'
 
 const style = {
     main: `flex flex-col p-[10px]`
@@ -32,7 +33,7 @@ useEffect(() => {
                 <Message key={message.id} message={message}/>
             ))}
         </main>
-            {/* Send Message Component */}
+            <SendMessage scroll={scroll}/>
         <span ref={scroll}></span>
     </>
   )
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { auth } from '../firebase'
+
 
 const style = {
     message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
@@ -8,10 +10,16 @@ const style = {
 }
 
 export const Message = ({message}) => {
+
+  const messageClass =
+  message.uid === auth.currentUser.uid 
+  ?`${style.sent}`
+  : `${style.received}`
+
   return (
     <div>
-        <div className={style.message}>
-            <p className={style.name}>Dave</p>
+        <div className={`${style.message} ${messageClass}`}>
+            <p className={style.name}>{message.name}</p>
             <p>{message.text}</p>
         </div>
     </div>
